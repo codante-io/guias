5ee4e91c2a73455e48a778b29b8842a9f7c6d07b
Roberto Cestari <robertotcestari@gmail.com>
2025-10-27 10:50:00 -0300

    simplify app

--- apps/home/app/page.tsx
+++ apps/home/app/page.tsx
@@ -8,13 +8,16 @@ export default function Home() {
       <main className="relative flex h-full flex-col pt-6 px-4  sm:px-6 lg:px-8 container mx-auto">
         <Logo className="w-[200px] mx-auto md:ml-0" />
         <div className="text-center md:text-left">
-          <h1 className="text-3xl text-gray-600 font-lexend mt-10 md:mt-32">
+          <h1 className="text-3xl text-gray-600 font-lexend mt-10 md:mt-24">
             Guias do Codante
           </h1>
           <p className="text-gray-500 mt-2">
             Aqui você encontra todos os guias utilizados pelos nossos workshops!
-            Acesse o <a className='text-blue-500' href="https://codante.io">site do Codante</a>, ou se quiser contribuir, acesse o nosso
-            repositório no{' '}
+            Acesse o{' '}
+            <a className="text-blue-500" href="https://codante.io">
+              site do Codante
+            </a>
+            , ou se quiser contribuir, acesse o nosso repositório no{' '}
             <a
               className="text-blue-500"
               href="https://github.com/codante-io/guias"
